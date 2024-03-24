edb3109dd7c5d2a5327512ead130b67e63ba5123
Yassir C <51804657+Klimrod89@users.noreply.github.com>
2024-03-23 21:45:37 -0400

    Did a lot

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -1,3 +1,5 @@
+using MyBGList.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,12 +16,61 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    // app.UseDeveloperExceptionPage();
+}
+
+if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+    app.UseDeveloperExceptionPage(); 
+else
+{
+    app.UseExceptionHandler("/error");
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+// Error
+app.MapGet("/error", () => Results.Problem());
+app.MapGet("/error/test", () => { throw new Exception("test"); });
+
+app.MapGet("/person", () => new Person("Andrew", "Lock"));
+
+List<Person> people = new()
+    {
+        new("Tom", "Hanks"),
+        new("Denzel", "Washington"),
+        new("Leondardo", "DiCaprio"),
+        new("Al", "Pacino"),
+        new("Morgan", "Freeman"),
+    };
+
+app.MapGet("/person/{name}", (string name) => people.Where(p => p.FirstName.StartsWith(name)));
+
+// BoardGame
+app.MapGet("/boardgames", () => new BoardGame[] {
+                                                                    new () {
+                                                                        Id = 1,
+                                                                        Name = "Axis & Allies",
+                                                                        Year = 1981
+                                                                    },
+                                                                    new () {
+                                                                        Id = 2,
+                                                                        Name = "Citadels",
+                                                                        Year = 2000
+                                                                    },
+                                                                    new () {
+                                                                        Id = 3,
+                                                                        Name = "Terraforming Mars",
+                                                                        Year = 2016
+                                                                    }
+                                                                }
+                );
+
+
+//app.MapControllers();
 
 app.Run();
+
+
+public record Person(string FirstName, string LastName);
